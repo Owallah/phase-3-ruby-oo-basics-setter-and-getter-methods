93ed71457ee4d7e1024f0f34a3e6c39e5211f424
Owallah <mikesonowallah@gmail.com>
2023-05-18 12:35:53 +0300

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,24 @@
 class Person
 
+    def name= (name)
+        @name = name
+        
+    end
+
+    def name
+        @name
+    end
+
+    def job= (job)
+        @job = job
+    end
+
+    def job
+        @job
+    end
+
 end
+
+johnson = Person.new
+johnson.name = ('Johnson')
+puts johnson.name
